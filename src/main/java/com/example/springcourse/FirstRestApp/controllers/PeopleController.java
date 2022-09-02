1ec24e2f3bf29cb329fd6f05eaa09ec9edc9c376
Polina Grigorovich <grigorovichpk@mail.ru>
2022-09-02 16:02:37 +0300

    Initial commit

---
+++ src/main/java/com/example/springcourse/FirstRestApp/controllers/PeopleController.java
@@ -0,0 +1,8 @@
+package com.example.springcourse.FirstRestApp.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PeopleController {
+
+}
---
+++ src/main/java/com/example/springcourse/FirstRestApp/services/PeopleService.java
@@ -0,0 +1,30 @@
+package com.example.springcourse.FirstRestApp.services;
+
+import com.example.springcourse.FirstRestApp.models.Person;
+import com.example.springcourse.FirstRestApp.repositories.PeopleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class PeopleService {
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    public List<Person> fingAll() {
+        return peopleRepository.findAll();
+    }
+
+    public Person findOne(int id) {
+        Optional<Person> foundPerson = peopleRepository.findById(id);
+        return foundPerson.orElse(null);
+    }
+}
